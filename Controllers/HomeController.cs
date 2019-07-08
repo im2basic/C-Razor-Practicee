60b98f5bc92739063fc7bf22c9240a3973a77774
Anisong Chanthalalay <basically.tata@gmail.com>
2019-07-08 16:37:42 -0500

    done

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RazorFun.Models;
+
+namespace RazorFun.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Home()
+        {
+            return View("Home");
+        }
+
+    }
+}
